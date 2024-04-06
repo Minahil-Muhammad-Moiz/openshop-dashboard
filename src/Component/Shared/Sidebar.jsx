6f067944c08d9c8e217bc4596db9450f0c8de075
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-06 15:07:34 +0500

    Completed Sidebar

--- src/Component/Shared/Sidebar.jsx
+++ src/Component/Shared/Sidebar.jsx
@@ -1,8 +1,12 @@
 import { FcShop } from 'react-icons/fc'
-import { DASHBOARD_SIDEBAR_LINKS } from '../../Lib/Constant/navs'
+import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../../Lib/Constant/navs'
 import { Link } from 'react-router-dom'
 import classNames from 'classnames'
 import { useLocation } from 'react-router-dom'
+import { HiOutlineLogout } from 'react-icons/hi'
+
+const classlinks =
+    'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
 
 const Sidebar = () => {
     return (
@@ -13,24 +17,37 @@ const Sidebar = () => {
             </div>
             <div className="flex-1 flex flex-col py-5 gap-0.5">
                 {DASHBOARD_SIDEBAR_LINKS.map((item) => (
-                    <SidebarLink key={item.key} item={item}/>
+                    <SidebarLink key={item.key} item={item} />
                 ))}
             </div>
-            <div>Bottom Part</div>
+            <div className='flex flex-col gap-0.5 pt-2 border-t border-neutral-700'>
+                {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) => (
+                    <SidebarLink key={item.key} item={item} />
+                ))}
+                <div className={classNames( 'text-red-500 cursor-pointer',classlinks)} >
+                    <span><HiOutlineLogout/></span>
+                    Logout
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Sidebar
 
-const classlinks = "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
+const SidebarLink = ({ item }) => {
+    const { pathname } = useLocation()
 
-const SidebarLink = ({item})=>{
-  const {pathname} = useLocation()
-
-  return(
-  <Link to={item.path} className={classNames(pathname === item.path? 'bg-neutral-600 text-white': 'text-neutral-400',classlinks)} >
-    <span>{item.icon}</span>
-    {item.label}
-  </Link>
-  )}
+    return (
+        <Link
+            to={item.path}
+            className={classNames(
+                pathname === item.path ? 'bg-neutral-600 text-white' : 'text-neutral-400',
+                classlinks
+            )}
+        >
+            <span>{item.icon}</span>
+            {item.label}
+        </Link>
+    )
+}
