526fbd7fa3c43a5694f35e1ea1f6e3d8339c9c04
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-06 12:20:49 +0500

    made Sidebar

--- src/Component/Shared/Layout.jsx
+++ src/Component/Shared/Layout.jsx
@@ -1,14 +1,16 @@
-import {Outlet} from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
+import Sidebar from './Sidebar'
 
 const Layout = () => {
-  return (
-    <div>
-        <div className='bg-sky-100'>Sidebar</div>
-        <div className='bg-teal-200'>Header</div>
-        <div>{<Outlet/>}</div>
-        <div>Footer</div>
-    </div>
-  )
+    return (
+        <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+            <Sidebar/>
+            <div className='p-4'>
+                <div className="bg-teal-200">Header</div>
+                <div>{<Outlet />}</div>
+            </div>
+        </div>
+    )
 }
 
 export default Layout
---
+++ src/Component/Shared/Sidebar.jsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col text-white bg-neutral-900 w-60 p-3">
+      <div></div>
+      <div className="flex-1" >Top Part</div>
+      <div>Bottom Part</div>
+    </div>
+  )
+}
+
+export default Sidebar
