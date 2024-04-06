cd20d6d01821c597c3a7dc8b1aea553aff8ed319
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-06 13:22:10 +0500

    added Sidebar Links

--- src/Component/Shared/Sidebar.jsx
+++ src/Component/Shared/Sidebar.jsx
@@ -1,11 +1,30 @@
+import { FcShop } from 'react-icons/fc'
+import { DASHBOARD_SIDEBAR_LINKS } from '../../Lib/Constant/navs'
+import { Link } from 'react-router-dom'
+
 const Sidebar = () => {
-  return (
-    <div className="flex flex-col text-white bg-neutral-900 w-60 p-3">
-      <div></div>
-      <div className="flex-1" >Top Part</div>
-      <div>Bottom Part</div>
-    </div>
-  )
+    return (
+        <div className="flex flex-col text-white bg-neutral-900 w-60 p-3">
+            <div className="flex items-center gap-2 px-1 py-3">
+                <FcShop fontSize={24} />
+                <span className="text-lg text-neutral-100">OpenShop</span>
+            </div>
+            <div className="flex-1">
+                {DASHBOARD_SIDEBAR_LINKS.map((item) => (
+                    <SidebarLink key={item.key} item={item}/>
+                ))}
+            </div>
+            <div>Bottom Part</div>
+        </div>
+    )
 }
 
 export default Sidebar
+
+
+const SidebarLink = ({item})=>(
+  <Link to={item.path}>
+    <span>{item.icon}</span>
+    {item.label}
+  </Link>
+)
