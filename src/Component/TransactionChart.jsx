1e766e711020f7ff5721265b88cbbdb9d046ac67
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-07 15:50:48 +0500

    fixed the height of the bar chart

--- src/Component/TransactionChart.jsx
+++ src/Component/TransactionChart.jsx
@@ -5,7 +5,7 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsive
 
 const TransactionChart= ()=> {
 	return (
-		<div className="h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
+		<div className="h-[20rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
 			<strong className="text-gray-700 font-medium">Transactions</strong>
 			<div className="mt-3 w-full h-full flex-1 text-xs ">
 				<ResponsiveContainer width="100%" height="100%" className="min-h-[15rem]">
