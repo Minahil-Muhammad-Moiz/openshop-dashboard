211d3325e89c0079d7816da3479cecfae4185c5a
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-06-20 17:46:14 +0500

    deployment

--- src/Component/Shared/Sidebar.jsx
+++ src/Component/Shared/Sidebar.jsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom'
 import classNames from 'classnames'
 import { useLocation } from 'react-router-dom'
 import { HiOutlineLogout } from 'react-icons/hi'
+import { useState } from 'react'
 
 const classlinks =
     'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
 
 const Sidebar = () => {
+
     return (
         <div className="flex flex-col text-white bg-neutral-900 w-[15%] p-3 h-screen fixed z-10 ">
             <div className="flex items-center gap-2 px-1 py-3">
